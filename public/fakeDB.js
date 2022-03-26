efd9c539963c222a5fa05c6aad03fa82cc0a2152
SadikHasanRafi <sadikkhasan@gmail.com>
2022-03-26 12:48:41 +0600

    api created

---
+++ public/fakeDB.js
@@ -0,0 +1,86 @@
+const re = [{
+        id: 121,
+        name: 'Micropack K-206',
+        price: 450,
+        brand: 'Micropack',
+        img: './k-206-01-500x500.jpg'
+    },
+    {
+        id: 122,
+        name: 'Astrum KB080',
+        price: 450,
+        brand: 'Astrum',
+        img: './kb080-01-500x500.jpg'
+    },
+    {
+        id: 123,
+        name: 'Delux OM02',
+        price: 500,
+        brand: 'Delux',
+        img: './om-02u-500x500.jpg'
+    },
+    {
+        id:124,
+        name:'Xtreme K911',
+        price:500,
+        brand:"Xtreme",
+        img:'./k911-01-500x500.jpg'
+    },
+    {
+        id:125,
+        name:'Delux K6006',
+        price:500,
+        brand:'Delux',
+        img:'./ka6006-01-500x500.jpg'
+    },
+    {
+        id:126,
+        name:'Golden Field GF-k301',
+        price:500,
+        brand:'Golden Field',
+        img:'./golden-field-gf-k301-wired-multimedia-keyboard-500x500.jpg'
+    },
+    {
+        id:127,
+        name:'Walton WKS005WN',
+        price:550,
+        brand:'Walton',
+        img:'./wks005wn-1-500x500.jpg'
+    },
+    {
+        id:128,
+        name:'Delux KA190U',
+        price:550,
+        brand:'Delux',
+        img:'./ka190u-01-500x500.jpg'
+    },
+    {
+        id:129,
+        name:'Golden Field GF-K101',
+        price:450,
+        brand:'Golden Field',
+        img:'./golden-field-gf-k101-wired-usb-keyboard-01-500x500.jpg'
+    },
+    {
+        id:130,
+        name:'A4TECH KRS-85',
+        price:500,
+        brand:'A4TECH',
+        img:'./krs-85-1-500x500.jpg'
+    },
+    {
+        id:131,
+        name:'Astrum KB170',
+        price:550,
+        brand:'Astrum',
+        img:'./kb170-01-500x500.jpg'
+    },
+    {
+        id:132,
+        name:'Astrum KB350 Mini',
+        price:600,
+        brand:'Astrum',
+        img:'./kb350-01-500x500.jpg'
+    }
+
+]
